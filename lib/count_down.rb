beba0ab40eeb6e25665484f089ab02b0b5a9391e
Alexa Gamil <alexis_gamil99@yahoo.com>
2020-08-27 17:39:10 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,8 +1,7 @@
-count = 10 
-greeting = "Happy New Year!"
+count = 10  
 while (count >= 0) do
   puts "#{count}"
   count -= 1
   break if count == 0 
-  puts "Happy New Year!"
 end
+puts "Happy New Year!"
